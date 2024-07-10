a85fe12a6a9acff2da18dd18d951730d451f4304
Kiều <kieudpqn@yahoo.com>
2024-07-10 22:11:40 +0700

    Initial | #1 Create abstract class Piece

---
+++ script.js
@@ -0,0 +1,16 @@
+class Position {
+    constructor(row, col) {
+        this.row = row;
+        this.col = col;
+    }
+}
+
+class Piece {
+    constructor(position, color) {
+        this.position = position;
+        this.color = color;
+    }
+    isValidMove(newPosition, board) {
+
+    }
+}
